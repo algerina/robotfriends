06a7c64de21567b26c82b78d2d313de45a02521a
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-12-19 16:51:07 +0100

    Create Robot component

---
+++ src/Robots.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export const robots = [
+    {
+        id: 1,
+        name: 'Mimi',
+        username: 'Mi',
+        email: 'drole@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Mido',
+        username: 'Toto',
+        email: 'mito@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Redvan',
+        username: 'Kwasa',
+        email: '3dess@chebka.com'
+    },
+    {
+        id: 4,
+        name: 'Lina',
+        username: 'Chichi',
+        email: 'tabouza@booz.edu'
+    },
+    {
+    id: 5,
+    name: 'Miro',
+    username: 'Arouss',
+    email: 'kawkaw@tik.biz'
+    }
+]
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
+import { robots } from './Robots';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email} />
   </React.StrictMode>,
   document.getElementById('root')
 );
